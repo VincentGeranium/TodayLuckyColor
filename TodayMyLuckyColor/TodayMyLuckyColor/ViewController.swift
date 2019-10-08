d24f14bd789815c3136f9aac1d38294f760fb059
김광준 <kwangjun3952@gmail.com>
2019-10-08 16:58:26 +0900

    project init

---
+++ TodayMyLuckyColor/TodayMyLuckyColor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodayMyLuckyColor
+//
+//  Created by 김광준 on 08/10/2019.
+//  Copyright © 2019 VincentGeranium. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

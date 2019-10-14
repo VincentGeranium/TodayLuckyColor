d214961efb37be001747303066a565a5355d1b2c
김광준 <kwangjun3952@gmail.com>
2019-10-15 00:16:33 +0900

    버튼, 레이블 만들고 올리기, ColorList 구조체 구현 중

---
+++ TodayMyLuckyColor/TodayMyLuckyColor/ColorList.swift
@@ -0,0 +1,15 @@
+//
+//  ColorList.swift
+//  TodayMyLuckyColor
+//
+//  Created by 김광준 on 2019/10/14.
+//  Copyright © 2019 VincentGeranium. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct ColorList {
+    static let green = UIColor.green
+    static let red = UIColor.red
+}
--- TodayMyLuckyColor/TodayMyLuckyColor/ViewController.swift
+++ TodayMyLuckyColor/TodayMyLuckyColor/ViewController.swift
@@ -9,11 +9,89 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let selectBtn = UIButton(type: .custom)
+    let colorLable = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        addColorLable()
+        btnConfigure()
+        addButton()
+        
+        
     }
+    
+    private func addColorLable() {
+        self.view.addSubview(colorLable)
+        
+        colorLable.translatesAutoresizingMaskIntoConstraints = false
+        
+        let guide = view.safeAreaLayoutGuide
+        let labelBottomConstant = (view.bounds.size.height - (view.bounds.size.height - 100))
+        
+        let labelTop: NSLayoutConstraint
+        labelTop = colorLable.topAnchor.constraint(equalTo: guide.topAnchor)
+        
+        let labelLeading: NSLayoutConstraint
+        labelLeading = colorLable.leadingAnchor.constraint(equalTo: guide.leadingAnchor)
+        
+        let labelTrailing: NSLayoutConstraint
+        labelTrailing = colorLable.trailingAnchor.constraint(equalTo: guide.trailingAnchor)
+        
+        let labelBottom: NSLayoutConstraint
+        labelBottom = colorLable.bottomAnchor.constraint(equalTo: guide.bottomAnchor, constant: -(labelBottomConstant))
+        
+        labelTop.isActive = true
+        labelLeading.isActive = true
+        labelTrailing.isActive = true
+        labelBottom.isActive = true
+        
+    }
+    
+    private func addButton() {
+        self.view.addSubview(selectBtn)
+        
+        selectBtn.translatesAutoresizingMaskIntoConstraints = false
+        
+        let guide = view.safeAreaLayoutGuide
+        
+        
+        let btnTop: NSLayoutConstraint
+        btnTop = selectBtn.topAnchor.constraint(equalTo: colorLable.bottomAnchor)
+        
+//        let btnCenterY: NSLayoutConstraint
+//        btnCenterY = selectBtn.centerYAnchor.constraint(equalTo: guide.centerYAnchor)
+        
+        let btnLeading: NSLayoutConstraint
+        btnLeading = selectBtn.leadingAnchor.constraint(equalTo: guide.leadingAnchor, constant: 20)
+        
+        let btnTrailing: NSLayoutConstraint
+        btnTrailing = selectBtn.trailingAnchor.constraint(equalTo: guide.trailingAnchor, constant: -20)
+        
+        let btnBottom: NSLayoutConstraint
+        btnBottom = selectBtn.bottomAnchor.constraint(equalTo: guide.bottomAnchor)
+        
+        btnTop.isActive = true
+//        btnCenterY.isActive = true
+        btnLeading.isActive = true
+        btnTrailing.isActive = true
+        btnBottom.isActive = true
+    }
+    
+    private func btnConfigure() {
+        selectBtn.setTitle("touch", for: .normal)
+        selectBtn.titleLabel?.font = UIFont.systemFont(ofSize: 30)
+        selectBtn.setTitleColor(.red, for: .normal)
+//        selectBtn.backgroundColor = .white
+        selectBtn.addTarget(self, action: #selector(didTppedSelectBtn(_:)), for: .touchUpInside)
+    }
+    
+    @objc private func didTppedSelectBtn(_ sender: UIButton) {
+        print("tapped Btn")
+    }
+    
 
 
 }
